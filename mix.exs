17ac5b22343866455fbc21b122f67690938231fc
Operable Engineering <engineering@operable.io>
2017-02-22 14:18:54 -0500

    Updated app version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Probe.Mixfile do
 
   def project do
     [app: :probe,
-     version: "1.0.0-beta.3",
+     version: "1.0.0-beta.4",
      elixir: "~> 1.3.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
